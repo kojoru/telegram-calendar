b7bff637d1157fdced9b3fbe9c51a937a27e7eb2
Konstantin Yakushev <kojo@kojo.dev>
2023-10-07 22:54:45 +0200

    Implement message handling

--- index.js
+++ index.js
@@ -11,57 +11,31 @@ router.get('/', () => {
 	return new Response('Hello, world! This is the root page of your Worker template.');
 });
 
-/*
-This route demonstrates path parameters, allowing you to extract fragments from the request
-URL.
 
-Try visit /example/hello and see the response.
-*/
-router.get('/example/:text', ({ params }) => {
-	// Decode text like "Hello%20world" into "Hello world"
-	let input = decodeURIComponent(params.text);
+router.post('/telegramMessage', async (request, env) => {
 
-	// Serialise the input into a base64 string
-	let base64 = btoa(input);
+	let json = await request.json();
+	const telegramProvidedToken = request.headers.get('X-Telegram-Bot-Api-Secret-Token');
+	const savedToken = await env.DB.prepare(
+        "SELECT value FROM settings WHERE name = ?"
+      )
+        .bind("telegram_security_code")
+        .first('value');
 
-	// Return the HTML with the string to the client
-	return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
-		headers: {
-			'Content-Type': 'text/html',
-		},
-	});
-});
-
-/*
-This shows a different HTTP method, a POST.
-
-Try send a POST request using curl or another tool.
-
-Try the below curl command to send JSON:
-
-$ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
-*/
-router.post('/post', async request => {
-	// Create a base object with some fields.
-	let fields = {
-		asn: request.cf.asn,
-		colo: request.cf.colo,
-	};
-
-	// If the POST data is JSON then attach it to our response.
-	if (request.headers.get('Content-Type') === 'application/json') {
-		let json = await request.json();
-		Object.assign(fields, { json });
+	if (telegramProvidedToken !== savedToken) {
+		console.log(`Token mismatch: ${telegramProvidedToken} !== ${savedToken}`);
+		return new Response('Unauthorized', { status: 401 });
+	} else {
+		console.log(`Token match: ${telegramProvidedToken} === ${savedToken}`);
 	}
 
-	// Serialise the JSON to a string.
-	const returnData = JSON.stringify(fields, null, 2);
+	// Serialize the JSON to a string.
+	const messageToSave = JSON.stringify(json);
+	await env.DB.prepare(
+		"INSERT INTO messages (message) VALUES (?)"
+	).bind(returnData).run();
 
-	return new Response(returnData, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
+	return new Response('Success', { status: 200 });
 });
 
 router.post('/init', async (request, env, ctx) => {
@@ -77,7 +51,6 @@ router.post('/init', async (request, env, ctx) => {
 
 	let token;
 	if (results.length === 0) {
-		// todo: less insane way of generating this
 		token = crypto.getRandomValues(new Uint8Array(16)).join("");
 		await env.DB.prepare(
 			"INSERT INTO settings (name, value) VALUES (?, ?)"
